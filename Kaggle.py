ea52b289b08d14ec08ce82a3bc4a6c38bd209e09
igor <bilyk.box@gmail.com>
2015-01-05 11:12:00 -0500

    Kaggle

--- Kaggle.py
+++ Kaggle.py
@@ -34,58 +34,74 @@ class Trip:
         self.coordinates = df
         self.n = df.shape[0]
         self.ID = int(fName.split('/')[-1].split('.')[0])
+        self.DropPrecision()
         self.RemoveDirectionOutliers()
-        self.getFeatures()
+        self.getRadius()
+        self.RemoveRadiusOutliers()
+        self.getSpeed()
         self.RemoveSpeedOutliers()
+        self.getFeatures()
         
     def getTripLen(self):
         self.n = self.coordinates.shape[0]
         self.tripDuration = (self.n-1)/60
         l = 0.0
         for i in range(1,self.n):
-            l+=self.Distance(self.coordinates.x[i],self.coordinates.y[i],self.coordinates.x[i-1],self.coordinates.y[i-1])
+            l+=self.DistanceInd(i-1,1)
         self.tripLen = l
+        
+    def getSpeed(self):
+        self.n = self.coordinates.shape[0]
+        
+        self.speed = pd.DataFrame(np.zeros((self.n-1,1)))
+        self.speed.columns = ['m_s']
+        
+        for i in range(self.n-1):
+            self.speed.m_s[i] = self.DistanceInd(i,1)
     
     def getFeatures(self):
         self.n = self.coordinates.shape[0]
         
         self.speed = pd.DataFrame(np.zeros((self.n-1,1)))
         self.speed.columns = ['m_s']
-        
-        self.radius = pd.DataFrame(np.zeros((self.n-2,1)))
-        self.radius.columns = ['m']
-        
+                
         self.features = pd.DataFrame(np.zeros((self.n-2,3)))
         self.features.columns = ['acc','cacc','v']
         
         i=0
-        self.speed.m_s[i] = self.Distance(\
-                                self.coordinates.x[i+1],self.coordinates.y[i+1],\
-                                self.coordinates.x[i],self.coordinates.y[i])
+        self.speed.m_s[i] = self.DistanceInd(i,1)
+        
         for i in range(1,self.n-1):
-            self.speed.m_s[i] = self.Distance(\
-                                self.coordinates.x[i+1],self.coordinates.y[i+1],\
-                                self.coordinates.x[i],self.coordinates.y[i])
-            
-            r = self.Radius(i-1)
-            self.radius.m[i-1] = r
+            self.speed.m_s[i] = self.DistanceInd(i,1)
             
             self.features.v[i-1]=0.5*(self.speed.m_s[i-1]+self.speed.m_s[i])
             
-            if r>1:
-                self.features.cacc[i-1] = pow(self.features.v[i-1],2)/r
+            if self.Radius(i-1)>1:
+                self.features.cacc[i-1] = pow(self.features.v[i-1],2)/self.Radius(i-1)
 
             self.features.acc[i-1] = self.speed.m_s[i]-self.speed.m_s[i-1]
     
     def Distance(self,x1,y1,x2,y2):
         return pow(pow(x1-x2,2)+pow(y1-y2,2),0.5)
+    
+    def DistanceInd(self,i,d):
+        return pow(pow(self.coordinates.x[i+d]-self.coordinates.x[i],2)+\
+                   pow(self.coordinates.y[i+d]-self.coordinates.y[i],2),0.5)
    
     def Radius(self,i):
-        a = self.Distance(self.coordinates.x[i],self.coordinates.y[i],self.coordinates.x[i+1],self.coordinates.y[i+1])
-        b = self.Distance(self.coordinates.x[i+2],self.coordinates.y[i+2],self.coordinates.x[i+1],self.coordinates.y[i+1])
-        c = self.Distance(self.coordinates.x[i],self.coordinates.y[i],self.coordinates.x[i+2],self.coordinates.y[i+2])
+        a = self.DistanceInd(i,1)
+        #self.Distance(self.coordinates.x[i],self.coordinates.y[i],self.coordinates.x[i+1],self.coordinates.y[i+1])
+        b = self.DistanceInd(i+1,1)
+        #self.Distance(self.coordinates.x[i+2],self.coordinates.y[i+2],self.coordinates.x[i+1],self.coordinates.y[i+1])
+        c = self.DistanceInd(i,2)
+        #self.Distance(self.coordinates.x[i],self.coordinates.y[i],self.coordinates.x[i+2],self.coordinates.y[i+2])
+        
         p = 0.5*(a+b+c)
-        return 0.25*a*b*c/pow(p*(p-a)*(p-b)*(p-c),0.5)
+        denom = p*(p-a)*(p-b)*(p-c)
+        if denom<=0:
+            return inf
+        else:
+            return 0.25*a*b*c/pow(denom,0.5)
     
     def Turn(self,w,v):
         norm = numpy.linalg.norm(v)
@@ -94,6 +110,14 @@ class Trip:
         m[1,0] = v.y/norm;m[1,1] = v.x/norm
         [xn,yn] = m.T*matrix(w).T
         return xn,yn
+    
+    def DropPrecision(self):
+        self.n = self.coordinates.shape[0]
+        for i in range(self.n-1):
+            if self.DistanceInd(i,1)<1.5:
+                self.coordinates.x[i+1] = self.coordinates.x[i]
+                self.coordinates.y[i+1] = self.coordinates.y[i]
+        
     
     def RemoveDirectionOutliers(self):
         self.n = self.coordinates.shape[0]
@@ -121,6 +145,40 @@ class Trip:
                     errors+=1
 
             if ((errors==0) | (iteration>10)):
+                break
+                
+    def getRadius(self):
+        n = self.n-2
+        self.radius = pd.DataFrame(np.zeros((n,1)))
+        self.radius.columns = ['m']
+        
+        for i in range(n):
+            self.radius.m[i] = self.Radius(i)
+                
+    def RemoveRadiusOutliers(self):
+        n = self.n-2
+        iteration = 0;
+        
+        while True:
+            iteration+=1
+            errors = 0
+            for i in range(n):                
+                if self.radius.m[i]==inf:
+                    continue
+                    
+                if (self.radius.m[i]<2):# | (self.radius.m[i]>500)):
+                    #print i
+                    self.coordinates.x[i+1] = 0.5*(self.coordinates.x[i]+self.coordinates.x[i+2])
+                    self.coordinates.y[i+1] = 0.5*(self.coordinates.y[i]+self.coordinates.y[i+2])
+                    self.radius.m[i] = inf
+                    if i>0:
+                        self.radius.m[i-1] = self.Radius(i-1)
+                    if i<n-1:
+                        self.radius.m[i+1] = self.Radius(i+1)
+
+                    errors+=1
+  
+            if ((errors==0) | (iteration>100)):
                 break
     
     def RemoveSpeedOutliers(self):
@@ -152,8 +210,8 @@ class Trip:
                     self.coordinates.x[i+1] += move*v[0]
                     self.coordinates.y[i+1] += move*v[1]
                     
-                    self.speed.m_s[i] = self.Distance(self.coordinates.x[i+1],self.coordinates.y[i+1],self.coordinates.x[i],self.coordinates.y[i])
-                    self.speed.m_s[i+1] = self.Distance(self.coordinates.x[i+1],self.coordinates.y[i+1],self.coordinates.x[i+2],self.coordinates.y[i+2])
+                    self.speed.m_s[i] = self.DistanceInd(i,1)
+                    self.speed.m_s[i+1] = self.DistanceInd(i+1,1)
                     
                     errors+=1
                     
@@ -169,8 +227,8 @@ class Trip:
                     self.coordinates.x[i] += move*v[0]
                     self.coordinates.y[i] += move*v[1]
                                    
-                    self.speed.m_s[i-1] = self.Distance(self.coordinates.x[i],self.coordinates.y[i],self.coordinates.x[i-1],self.coordinates.y[i-1])
-                    self.speed.m_s[i] = self.Distance(self.coordinates.x[i+1],self.coordinates.y[i+1],self.coordinates.x[i],self.coordinates.y[i])
+                    self.speed.m_s[i-1] = self.DistanceInd(i-1,1)
+                    self.speed.m_s[i] = self.DistanceInd(i,1)
                     
                     errors+=1
                     
@@ -180,15 +238,6 @@ class Trip:
                         r = self.Radius(j-1)
                         self.radius.m[j-1] = r
 
-                        self.features.v[j-1]=0.5*(self.speed.m_s[j-1]+self.speed.m_s[j])
-
-                        if r>1:
-                            self.features.cacc[j-1] = pow(self.features.v[j-1],2)/r
-                        else:
-                            self.features.cacc[j-1] = 0
-
-                        self.features.acc[j-1] = self.speed.m_s[j]-self.speed.m_s[j-1]
-
             if ((errors==0) | (iteration>10)):
                 break
     
@@ -197,22 +246,42 @@ class Trip:
 
 trips = []
 
-driverID = 1
+driverID = 20
 driverDir = '../Kaggle/drivers/'+str(driverID)
 tripFiles = os.listdir(driverDir)
 
 for index, tripFile in enumerate(tripFiles):
     df = pd.read_csv(driverDir+'/' + tripFile)
-    trips.append(Trip(1,driverDir+'/' + tripFile,df))  
+    trip = Trip(1,driverDir+'/' + tripFile,df)
+    trips.append(trip)  
 
     if index>2:
         break
 
 # <codecell>
 
+driversFiles = map(int,os.listdir('../Kaggle/drivers/'))
+driversFiles.sort()
+for driverID in driversFiles:
+    driverDir = '../Kaggle/drivers/'+str(driverID)
+    tripFiles = os.listdir(driverDir)
+    print driverID
+    for index, tripFile in enumerate(tripFiles):
+        if (tripFile.split('.')[0][-1]!='f'):  
+            df = pd.read_csv(driverDir+'/' + str(tripFile))
+            trip = Trip(1,driverDir+'/' + str(tripFile),df)
+            trip.features.to_csv(driverDir+'/' + str(trip.ID) + 'f.csv')
+            #print driverID, trip.ID
+
+# <codecell>
+
 tripInd = 1
-start = 110
-end = 150#trips[tripInd].n
+start = 0
+end = trips[tripInd].n
+
+# <codecell>
+
+trips[tripInd].Radius(46)
 
 # <codecell>
 
@@ -221,6 +290,10 @@ trips[tripInd].coordinates.ix[start:end,:]
 # <codecell>
 
 trips[tripInd].radius.ix[start:end,:]
+
+# <codecell>
+
+trips[tripInd].speed.ix[start:end,:]
 
 # <codecell>
 
@@ -251,7 +324,25 @@ geom_point(df.ix[start:end,:],aes(x='ind',y='cacc'),color='red',size=10)
 
 # <codecell>
 
-ggplot(trips[tripInd].features.ix[start:end,:],aes(x='acc',y='cacc')) + geom_point(size=10)
+ggplot(trips[tripInd].features[trips[tripInd].features.v>5].ix[start:end,:],aes(x='acc',y='cacc')) + geom_point(size=10)
+
+# <codecell>
+
+ggplot(trips[tripInd].features[trips[tripInd].features.v>5].ix[start:end,:],aes(x='acc')) + geom_histogram(binwidth = 0.1)
+
+# <codecell>
+
+(n,b,p) = matplotlib.pyplot.hist(list(trips[tripInd].features[trips[tripInd].features.v>5].acc),bins=100,range=[-5,5])
+
+# <codecell>
+
+df = pd.DataFrame(np.zeros((len(b)/2,2)))
+df.columns = ['x','y']
+df.x = b[len(b)/2+1:]
+_n = df.shape[0]
+for i in range(_n):
+    df.y[i] = (n[_n+i]-n[_n-i-1])/sum(n)
+ggplot(df,aes(x='x',y='y')) + geom_point(size=10) + geom_line() + geom_hline(yintercept=0) 
 
 # <codecell>
 
